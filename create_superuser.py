042db31c5f2ec03789eb6070ad066ec870ce0f70
Kelvin Keter <33624862+kelvinketer@users.noreply.github.com>
2025-12-19 19:41:12 +0300

    Add superuser script

---
+++ create_superuser.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laptop_place_project.settings')
+django.setup()
+
+from django.contrib.auth.models import User
+
+# Change these to whatever you want
+USERNAME = 'admin'
+PASSWORD = 'admin123_ChangeMe!'
+EMAIL = 'admin@example.com'
+
+if not User.objects.filter(username=USERNAME).exists():
+    print(f'Creating superuser: {USERNAME}')
+    User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
+else:
+    print('Superuser already exists.')
