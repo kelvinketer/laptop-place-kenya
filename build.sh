7a821c0f5ae05812ca162ee7ac3bd13e261e5021
Kelvin Keter <33624862+kelvinketer@users.noreply.github.com>
2025-12-20 15:10:56 +0300

    Fix missing static folder and force collectstatic

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -o errexit
 # 1. Install dependencies
 pip install -r requirements.txt
 
-# 2. Collect static files (The step that fixes the broken Admin styles)
-python manage.py collectstatic --no-input
+# 2. CLEAR and Collect static files (Fixes the 0 files copied error)
+python manage.py collectstatic --no-input --clear
 
-# 3. Migrate database (Ensures tables exist)
+# 3. Migrate database
 python manage.py migrate
